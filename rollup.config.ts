ff623fe54239b4ecfbf26937b69eb12236087f92
powerfulech0 <gregoryrearden@gmail.com>
2025-09-10 21:08:22 -0400

    fix: made rollup pipeline better

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,29 +1,42 @@
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import dts from 'rollup-plugin-dts';
+import packageJson from './package.json' with { type: 'json' };
 
 export default [
+  // ESM
   {
-    input: 'index.ts',
+    input: './index.ts',
+    output: {
+      dir: './dist/esm',
+      format: 'esm',
+      sourcemap: 'true',
+    },
     plugins: [
       typescript({
-        noForceEmit: true,
+        declaration: true,
+        declarationDir: './dist/esm/dts',
       }),
-      commonjs(),
       nodeResolve(),
     ],
-    output: [
-      {
-        dir: 'dist/cjs',
-        format: 'cjs',
-        sourcemap: 'true',
-      },
-      {
-        dir: 'dist/esm',
-        format: 'esm',
-        sourcemap: 'true',
-      },
-    ],
+    external: [/node_modules/],
+  },
+  // ESM (types)
+  {
+    input: './dist/esm/dts/index.d.ts',
+    output: [{ file: packageJson.types, format: 'es' }],
+    plugins: [dts()],
+  },
+  // CJS
+  {
+    input: './index.ts',
+    output: {
+      dir: './dist/cjs',
+      format: 'cjs',
+      sourcemap: 'true',
+    },
+    plugins: [typescript(), commonjs(), nodeResolve()],
     external: [/node_modules/],
   },
 ];
